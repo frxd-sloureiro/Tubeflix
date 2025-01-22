2a4bc1c678147e524f5b53d45e23bdc0ca30c453
frxd-sloureiro <soprazuarhuehue@gmail.com>
2025-01-22 13:59:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TubeFlix</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header><h1>TubeFlix</h1></header>
 </body>
 </html>
